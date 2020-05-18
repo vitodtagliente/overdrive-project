e74494fdfd3de53d506753173dcc03e02e37c07d
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2020-05-18 23:36:07 +0200

    changes

--- web/public/js/datatable.js
+++ web/public/js/datatable.js
@@ -68,158 +68,254 @@ class Pagination {
 }
 
 class Table {
+    /// The usage method
+    static get Mode() {
+        return {
+            Ajax: 'ajax',
+            Data: 'data'
+        };
+    }
     /// constructor
-    /// @param url - The base url for retrieving data
-    constructor(url) {
-        this.#url = url;
+    /// @param data - can be a static array of data or the base url for retrieving data
+    constructor(data) {
+        if (Array.isArray(data))
+        {
+            this.#mode = Table.Mode.Data;
+            this.#data = data;
+        }
+        else
+        {
+            this.#mode = Table.Mode.Ajax;
+            this.#url = data;
+        }
     }
 
+    /// Retrieve the columns of the table
+    /// @return - The columns
+    get columns() {
+        return this.#columns;
+    }
+
+    /// Set the columns
+    /// @param value - The columns
     set columns(value) {
-        this.#columns = value;
+        if (Array.isArray(value))
+        {
+            this.#columns = Array();
+            for (const column of value)
+            {
+                this.#columns[column] = column;
+            }
+        }
+        else 
+        {
+            this.#columns = value;
+        }
     }
 
+    /// Retrieve the data of the table
+    /// @return - The table
     get data() {
         return this.#data;
     }
 
+    /// Update the data
+    /// #param url - The new url on which request data
+    /// @return - The fetched data
     async fetch(url) {
-        /*
-        return await $.get(
-            url
-        );
-        */
-        this.#data = Array();
-        for (var i = 0; i < 50; ++i)
+        if (this.mode == Table.Mode.Data)
         {
-            this.#data.push({
-                id: i,
-                name: 'name' + i,
-                description: 'description' + i
-            });
+            return this.data;
         }
-        return this.#data;
+        else 
+        {
+            if (this.url != url)
+            {
+                this.#url = url;
+                this.#data = await $.get(
+                    url
+                );
+            }
+            return this.data;
+        }
     }
 
+    /// Register a custom renderer for a specific field
+    /// @param name - The name of the field
+    /// @param renderer - The render callback
     field(name, renderer) {
-        if (renderer != null)
+        this.#fields[name] = renderer;
+    }
+
+    /// Find a field renderer
+    /// @param name - The name of the field
+    /// @return - The renderer callback if exists
+    #findFieldRenderer = (name) => {
+        for (const field of Object.keys(this.#fields))
         {
-            this.#fieldRenderers[name] = renderer;
+            if (field == name)
+            {
+                return this.#fields[name];
+            }
         }
+        return null;
+    }
+
+    /// Retrieve the table mode
+    /// @return - The mode
+    get mode() {
+        return this.#mode;
+    }
+
+    /// Return the parent widget
+    get parent() {
+        return this.#dom.parent;
     }
 
     /// Render the datatable 
     /// @param id - The id of the DOM element in which render the datatable
     async render(id) {
-        const container = $(id);
-        if (container == null)
+        // create the table at the first time
+        if (this.parent == null)
         {
-            return;
+            this.#dom.parent = document.getElementById(id);
+            if (this.parent == null)
+            {
+                console.log('Unable to create the table! Invalid contained id');
+                return false;
+            }
+
+            // create the table
+            this.#dom.table = document.createElement('table');
+            for (const css_class of this.classes.table)
+            {
+                this.table.classList.add(css_class);
+            }
+            this.parent.append(this.table);
+
+            // setup the columns
+            if ((this.#columns == null 
+                || (Array.isArray(this.#columns) && this.#columns.length == 0))
+                && this.data.length > 0)
+            {
+                this.columns = Object.keys(this.data[0]);
+            }
+
+            // render the table head
+            await this.#renderHead();
         }
 
-        this.#table = document.createElement('table');
-        for (const css_class of this.classes.table)
-        {
-            this.#table.classList.add(css_class);
-        }
+        // render the table body
+        await this.#renderBody();
 
-        this.#data = await this.fetch(this.url);
-        const columns = this.#getColumns(this.#data.length > 0 ? this.#data[0] : null);
-        await this.renderHeader(columns);
-        await this.renderBody(columns, this.#data);
-
-        container.append(this.#table);
-
-        const paginationDiv = document.createElement('div');
-        console.log(await this.pagination.create(paginationDiv));
-        container.append(paginationDiv);
+        //const paginationDiv = document.createElement('div');
+        //console.log(await this.pagination.create(paginationDiv));
+        //container.append(paginationDiv);
     }
 
-    async renderHeader(columns) {
-        this.#table_head = this.#table.createTHead();
-        const row = this.#table_head.insertRow();
+    /// Render the body of the table
+    #renderBody = async () => {
+        // create the body if not exist or clear it
+        if (this.#dom.table_body == null)
+        {
+            this.#dom.table_body = this.table.createTBody();
+        }
+        else
+        {
+            this.#dom.table_body.innerHTML = '';
+        }
+
+        //const count = this.pagination.enabled
+        //    ? Math.min(data.length, this.pagination.limit)
+        //    : data.length;
+        const count = this.data.length;
+        const columns = Object.keys(this.columns);
+        for (let i = 0; i < count; ++i)
+        {
+            const model = data[i];
+            const row = this.#dom.table_body.insertRow();
+            row.setAttribute('id', model.id);
+            await this.renderRow(row, model, columns);
+        }
+    }
+
+    /// Render the column header
+    /// @param cell - The head row cell
+    /// @param name - The name of the column
+    renderColumn = async (cell, name) => {
+        cell.innerHTML = name;
+    }
+
+    /// Render the head of the table
+    #renderHead = async () => {
+        this.#dom.table_head = this.table.createTHead();
+        const row = this.#dom.table_head.insertRow();
+        const columns = this.columns;
         for (const column of Object.keys(columns))
         {
             const cell = row.insertCell();
-            cell.textContent = columns[column];
+            await this.renderColumn(cell, columns[column]);
             cell.setAttribute('scope', 'col');
         }
     }
 
-    async renderBody(columns, data) {
-        this.#table_body = this.#table.createTBody();
-        const count = this.pagination.enabled
-            ? Math.min(data.length, this.pagination.limit)
-            : data.length;
-        for (let i = 0; i < count; ++i)
-        {
-            const model = data[i];
-            const row = this.#table_body.insertRow();
-            row.setAttribute('id', model.id);
-            await this.renderRow(row, model, Object.keys(columns));
-        }
-    }
-
-    get url() {
-        return this.#url;
-    }
-
-    #getColumns = (model) => {
-        if (this.#columns == null
-            || (Array.isArray(this.#columns) && this.#columns.length == 0)
-            || Object.keys(this.#columns).length == 0)
-        {
-            let columns = {};
-            for (const column of Object.keys(model))
-            {
-                columns[column] = column;
-            }
-            return columns;
-        }
-        else if (Array.isArray(this.#columns))
-        {
-            let columns = {};
-            for (const column of this.#columns)
-            {
-                columns[column] = column;
-            }
-            return columns;
-        }
-        return this.#columns;
-    }
-
-    classes = {
-        col: Array(),
-        row: Array(),
-        table: ['table', 'table-hover']
-    };
-    pagination = new Pagination();
+    /// Render a row in the table
+    /// @param row - The table row
+    /// @param model - the model
+    /// @param fields - The columns of the table
     renderRow = async (row, model, fields) => {
         for (const field of fields)
         {
             const cell = row.insertCell();
-            let found = false;
-            for (const name of Object.keys(this.#fieldRenderers))
+            const renderer = this.#findFieldRenderer(field);
+            if (renderer != null)
             {
-                if (name == field)
-                {
-                    await this.#fieldRenderers[name](cell, model);
-                    found = true;
-                    break;
-                }
+                await renderer(cell, model);
             }
-
-            if (!found)
+            else 
             {
                 cell.innerHTML = model[field];
             }
         }
     };
 
+    /// Retrieve the DOM table
+    /// @return - The DOM table
+    get table() {
+        return this.#dom.table;
+    }
+
+    /// Retrieve the url
+    /// #return - The url
+    get url() {
+        return this.#url;
+    }
+
+    /// let to customize the table css per element
+    /// basic bootstrap classes by default
+    classes = {
+        col: Array(),
+        row: Array(),
+        table: ['table', 'table-hover']
+    };
+    pagination = new Pagination();
+
+    /// The columns of the table
+    /// can contains the showed name
     #columns = Array();
+    /// The fetched data
     #data = null;
-    #fieldRenderers = Array();
-    #table = null;
-    #table_body = null;
-    #table_head = null;
+    /// The DOM elements
+    #dom = {
+        parent: null,
+        table: null,
+        table_body: null,
+        table_head: null
+    };
+    /// Collection of fields renderers
+    #fields = Array();
+    /// The mode of the table
+    #mode = null;
+    /// The url for Ajax mode
     #url = null;
 }
