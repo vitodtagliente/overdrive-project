65b77053be30921b663404b94a3336ed7a23325c
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2020-05-27 22:44:04 +0200

    changes

--- web/controllers/auth_controller.js
+++ web/controllers/auth_controller.js
@@ -1,11 +1,20 @@
 const Controller = require('overdrive').Controller;
+const Session = require('overdrive').Session;
 
 class AuthController extends Controller {
     /// Serve the sign-in page
     /// @param req - The http request
     /// @param res - The http response
     static async signin(req, res) {
-        res.render('overdrive/signin', { signin: true });
+        const session = new Session(req);
+        if (session.user)
+        {
+            res.render('overdrive/leavesession', session.user);
+        }
+        else 
+        {
+            res.render('overdrive/signin', { signin: true });
+        }
     }
 
     /// Serve the sign-up page
