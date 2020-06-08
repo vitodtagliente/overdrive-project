6b5e147bccc240243a4a3108c4e27934e57c93f0
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2020-06-08 20:29:47 +0200

    changes

--- packages/mango/index.js
+++
@@ -1,4 +0,0 @@
-exports.Connection = require('./connection');
-exports.DataProvider = require('./data_provider');
-exports.Model = require('./model');
-exports.Seeder = require('./seeder');
--- packages/overdrive/index.js
+++ packages/overdrive/index.js
@@ -2,11 +2,11 @@ exports.Application = require('./application');
 exports.Auth = require('./auth');
 exports.Controller = require('./controller');
 exports.CRUD = require('./crud');
+exports.Database = require('overdrive-db');
 exports.IO = {
     Directory: require('./io/directory')
 };
 exports.Logger = require('overdrive-logger');
-exports.mango = require('mango');
 exports.Password = require('./password');
 exports.Respond = require('./respond');
 exports.Session = require('./session');
