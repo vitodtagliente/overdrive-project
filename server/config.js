0952b642096a415e49dcfa46ffc4facb2fc7d3fa
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2020-07-18 10:45:45 +0200

    added react client

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ module.exports = {
     CONNECTION: process.env.CONNECTION || 'mongodb://127.0.0.1/mrpg',
     CONNECTION_TYPE: process.env.CONNECTION_TYPE || 'mongodb',
     ENV: process.env.ENV || "development",
-    PORT: process.env.PORT || 3000,
+    PORT: process.env.PORT || 9000,
     SECRET: process.env.SECRET || 'OVERDRIVE-SECRET',
-    URL: process.env.URL || "http://localshost:3000"
+    URL: process.env.URL || "http://localshost:9000"
 };
