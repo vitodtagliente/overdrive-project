e3293735db6fb739d5a19be3b4e415cbc2d1c5b4
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2020-07-22 12:52:24 +0200

    Update Sidebar.js

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -3,34 +3,39 @@ import './Sidebar.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircle, faPowerOff } from "@fortawesome/free-solid-svg-icons";
 
-class SidebarUser extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+function User(props) {
+    return (
+        <div className="sidebar-item sidebar-header d-flex flex-nowrap">
+            <div className="user-pic">
 
-        };
-    }
-
-    render() {
-        return (
-            <div className="sidebar-item sidebar-header d-flex flex-nowrap">
-                <div className="user-pic">
-
-                </div>
-                <div className="user-info">
-                    <span className="user-name">{this.props.name}</span>
-                    <span className="user-name">
-                        <strong>{this.props.surname}</strong>
-                    </span>
-                    <span className="user-role">{this.props.role}</span>
-                    <span className="user-status">
-                        <FontAwesomeIcon icon={faCircle} />
-                        <span> Online</span>
-                    </span>
-                </div>
             </div>
-        );
-    }
+            <div className="user-info">
+                <span className="user-name">{props.name}</span>
+                <span className="user-name">
+                    <strong>{props.surname}</strong>
+                </span>
+                <span className="user-role">{props.role}</span>
+                <span className="user-status">
+                    <FontAwesomeIcon icon={faCircle} />
+                    <span> Online</span>
+                </span>
+            </div>
+        </div>
+    );
+}
+
+function Brand(props) {
+    return (
+        <div className="sidebar-item sidebar-brand">
+            <a href="/">{props.name}</a>
+        </div>
+    );
+}
+
+function LogoutModal(props) {
+    return (
+        <div></div>
+    );
 }
 
 export default class Sidebar extends React.Component {
@@ -41,7 +46,7 @@ export default class Sidebar extends React.Component {
     }
 
     componentDidMount() {
-        
+
     }
 
     componentDidUpdate() {
@@ -52,10 +57,8 @@ export default class Sidebar extends React.Component {
         return (
             <nav id="sidebar" className="sidebar-wrapper">
                 <div className="sidebar-content">
-                    <div className="sidebar-item sidebar-brand">
-                        <a href="/">{this.props.brand}</a>
-                    </div>
-                    <SidebarUser name="Vito Domenico" surname="Tagliente" role="Administrator" />
+                    <Brand name="Dashboard" />
+                    <User name="Vito Domenico" surname="Tagliente" role="Administrator" />
                 </div>
                 <div className="sidebar-footer">
                     <div>
