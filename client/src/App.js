f5b28ab8467cd04858531a8e40eb6a3c112d0c27
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2020-07-25 10:54:43 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,9 @@
 import React from 'react';
-import DataProvider from './DataProvider';
 import Dashboard from './components/Dashboard';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-class App extends React.Component {
-  #dataProvider = new DataProvider('http://localhost:9000/api/items');
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: []
-    };
-  }
-
-  componentDidMount() {
-    // fetch the data
-    this.#dataProvider.getList().then(res => {
-      this.setState({ items: res.data.data });
-    }).catch(e => {
-      console.log(e);
-    });
-  }
-
-  componentDidUpdate() {
-
-  }
-
-  render() {
-    return (
-      <Dashboard />
-    );
-  }
+export default function App(props) {
+  return (
+    <Dashboard />
+  );
 }
-
-export default App;
