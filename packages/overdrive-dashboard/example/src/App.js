9a89004849185cb68568c365ad2eed49f4453084
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2020-08-03 12:52:08 +0200

    Create overdrive-dashboard

---
+++ packages/overdrive-dashboard/example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'overdrive-dashboard'
+import 'overdrive-dashboard/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
