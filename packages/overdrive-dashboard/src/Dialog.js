c172ada0b69f8a407306573858dbf92e4bf9e529
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2020-08-13 18:44:43 +0200

    Update Dialog.js

--- packages/overdrive-dashboard/src/Dialog.js
+++ packages/overdrive-dashboard/src/Dialog.js
@@ -1,13 +1,39 @@
 import React from 'react';
 
 export default class Dialog extends React.Component {
+
+    #id = null;
+
     constructor(props) {
         super(props);
+        this.#id = Date.now();
+    }
+
+    get id() {
+        return this.#id;
     }
 
     render() {
         return (
-            <div></div>
+            <div className="modal fade" id={this.id} data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">{this.props.title}</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            {this.props.children}
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                            <button type="button" className="btn btn-primary">Understood</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
