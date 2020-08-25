ce2ed21f666cb29c3f8424215613be40d0fe12f4
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2020-08-25 17:13:18 +0200

    changes

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import style from './Login.css';
 
 export default class Login extends React.Component {
     constructor(props) {
@@ -8,7 +9,20 @@ export default class Login extends React.Component {
 
     render() {
         return (
-            <div>Login</div>
+            <form className={style['form-signin']}>
+                <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+                <label for="inputEmail" class="sr-only">Email address</label>
+                <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
+                <label for="inputPassword" className="sr-only">Password</label>
+                <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
+                <div class="checkbox mb-3">
+                    <label>
+                        <input type="checkbox" value="remember-me" /> Remember me
+                    </label>
+                </div>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                <p class="mt-5 mb-3 text-muted">&copy; 2017-2020</p>
+            </form>
         );
     }
 }
