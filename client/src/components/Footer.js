9b5333dc50873d5626ed8eadec2dc6edadb167a7
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2020-08-28 15:44:58 +0200

    changes

---
+++ client/src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import style from './Footer.css';
+
+export default class Footer extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    handleLogin(e) {
+
+    }
+
+    render() {
+        return (
+            <div className="footer">
+
+            </div>
+        );
+    }
+}
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Button } from 'overdrive-dashboard';
 import style from './Login.css';
+import Footer from './Footer';
 
 export default class Login extends React.Component {
     constructor(props) {
@@ -7,22 +9,36 @@ export default class Login extends React.Component {
 
     }
 
+    handleLogin(e) {
+
+    }
+
     render() {
         return (
-            <form className={style['form-signin']}>
-                <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-                <label for="inputEmail" class="sr-only">Email address</label>
-                <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
-                <label for="inputPassword" className="sr-only">Password</label>
-                <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
-                <div class="checkbox mb-3">
-                    <label>
-                        <input type="checkbox" value="remember-me" /> Remember me
+            <Fragment>
+                <div className="login">
+                    <form>
+                        <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+                        <div className="form-group">
+                            <input type="email" id="inputEmail" className="form-control rounded-0" placeholder="Email address" required autofocus />
+                        </div>
+                        <div className="form-group">
+                            <input type="password" id="inputPassword" className="form-control rounded-0" placeholder="Password" required />
+                        </div>
+                        <div class="checkbox mb-3">
+                            <label>
+                                <input type="checkbox" value="remember-me" /> Remember me
                     </label>
+                        </div>
+                        <Button
+                            name="Sign in"
+                            type="submit"
+                            onClick={(e) => this.handleLogin(e)}
+                        />
+                    </form>
                 </div>
-                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-                <p class="mt-5 mb-3 text-muted">&copy; 2017-2020</p>
-            </form>
+                <Footer />
+            </Fragment>
         );
     }
 }
