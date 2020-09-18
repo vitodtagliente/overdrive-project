9ad678f458678aba26c0379e8a7b3caf98d08669
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2020-09-18 17:04:02 +0200

    changes

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const Controller = require('overdrive').Controller;
 const Directory = require('overdrive').IO.Directory;
 const Logger = require('overdrive').Logger;
 const path = require('path');
-const Server = require('overdrive-socket').Server;
+const Server = require('overdrive-websocket').Server;
 
 const app = new Application();
 app.initialize(config);
